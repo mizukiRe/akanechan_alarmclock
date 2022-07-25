63e84174d0f6c175f8612eb241c48035514d7427
mizukiRe <midukihimmel@gmail.com>
2022-07-25 22:41:42 +0900

    茜ちゃんがタイマーより前に起きたら褒めてくれる機能を実装した

--- akanechanMain.py
+++ akanechanMain.py
@@ -16,17 +16,69 @@ GPIO.setup(24, GPIO.IN)
 
 # （１）茜ちゃんが朝起こしてくれる（cron起動予定）
 # （２）タクトスイッチを押下で停止
-# （３）トグルスイッチがオフの状態で停止
-# （４）1/1000くらいで朝から抜いてくれるシチュを追加（１０分程度で）
+# （３）スライドスイッチがオフの状態で停止
+# （４）1/1000くらいで朝からえっちなことしてくれるシチュを追加（１０分程度で）
 
 MP3_PATH = '/home/pi/Music/akanechan/akanechan_ohayou_01.mp3'
 
-# ローカルな現在の日付と時刻を取得
-dt = datetime.datetime.today()
-# 現在の時間を保存
-t = dt.time()
-print(t)  # 15:58:08.356501
+start_time = datetime.datetime.now()
+# whileの条件でエラーなるのでとりあえずtrueになるように変数にセット
+dt = datetime.datetime.now()
 
+now = datetime.datetime.now()
+
+aaa = dt + datetime.timedelta(hours=1)
+print(aaa)
+
+# 開始時刻＋１hまではスライドスイッチをオフを受け付ける
+# cronは実際の目覚ましの時間-1hを設定する想定
+while(start_time + datetime.timedelta(hours=1) > dt):
+
+    # ローカルな現在の日付と時刻を取得
+    dt = datetime.datetime.now()
+    # 現在の時間を保存
+    t = dt.time()
+    print(t)  # 15:58:08.356501
+    
+    print("茜ちゃんが褒めてくれる")
+
+    try:
+        if GPIO.input(24) == GPIO.HIGH:
+            GPIO.output(25, GPIO.HIGH)
+            print("akanechan")
+            sleep(10)
+            
+        else:
+            # スライドスイッチがOFFになったときの挙動
+            music_pass = "/home/pi/Music/akanechan/amaamahome/"
+            GPIO.output(25, GPIO.LOW)
+            
+            # ファイル名を取得
+            files = os.listdir(music_pass)
+            print("ファイル数:",str(len(files)))
+            for file in files:
+                print("\t",file)
+                
+            # 乱数で選曲
+            r = random
+            r.seed()
+            play_no = r.randint(0, len(files)-1)
+            print(play_no,"曲目を再生")
+            file = files[play_no]
+
+            fullpass = music_pass + file
+
+            args = ['omxplayer', '-o', 'alsa', fullpass]
+
+            process = subprocess.Popen(args)
+            sleep(60)
+            GPIO.cleanup()
+            exit()
+
+    except KeyboardInterrupt:
+        pass
+
+# あとでプログラム自体を分ける
 if ( t > datetime.time(13,00,00) and t < datetime.time(14,00,00)):
     music_pass = "/home/pi/Music/akanechan/ohiru/"
     print("false")
