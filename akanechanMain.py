30a5d1ff3d634b225d7690874eea496147769e86
mizukiRe <midukihimmel@gmail.com>
2022-08-10 13:50:22 +0900

    ランダムでファイルパスを生成する部分を関数化、リファクタリングしたで〜

--- akanechanMain.py
+++ akanechanMain.py
@@ -19,6 +19,24 @@ GPIO.setup(24, GPIO.IN)
 # （３）スライドスイッチがオフの状態で停止
 # （４）1/1000くらいで朝からえっちなことしてくれるシチュを追加（１０分程度で）
 
+# 引数のディレクトリにある音声ファイルをランダムで選んでフルパスを返す
+def getRandomVoiceFullFilepass(music_pass):
+    # ファイル名を取得
+    files = os.listdir(music_pass)
+    print("ファイル数:",str(len(files)))
+    for file in files:
+        print("\t",file)
+        
+    # 乱数で選曲
+    r = random
+    r.seed()
+    play_no = r.randint(0, len(files)-1)
+    print(play_no,"曲目を再生")
+    file = files[play_no]
+
+    fullpass = music_pass + file
+    return fullpass
+
 MP3_PATH = '/home/pi/Music/akanechan/akanechan_ohayou_01.mp3'
 
 start_time = datetime.datetime.now()
@@ -26,9 +44,6 @@ start_time = datetime.datetime.now()
 dt = datetime.datetime.now()
 
 now = datetime.datetime.now()
-
-aaa = dt + datetime.timedelta(hours=1)
-print(aaa)
 
 # 開始時刻＋１hまではスライドスイッチをオフを受け付ける
 # cronは実際の目覚ましの時間-1hを設定する想定
@@ -53,20 +68,7 @@ while(start_time + datetime.timedelta(hours=1) > dt):
             music_pass = "/home/pi/Music/akanechan/amaamahome/"
             GPIO.output(25, GPIO.LOW)
             
-            # ファイル名を取得
-            files = os.listdir(music_pass)
-            print("ファイル数:",str(len(files)))
-            for file in files:
-                print("\t",file)
-                
-            # 乱数で選曲
-            r = random
-            r.seed()
-            play_no = r.randint(0, len(files)-1)
-            print(play_no,"曲目を再生")
-            file = files[play_no]
-
-            fullpass = music_pass + file
+            fullpass = getRandomVoiceFullFilepass(music_pass)
 
             args = ['omxplayer', '-o', 'alsa', fullpass]
 
@@ -86,21 +88,7 @@ else:
     print("true")
     music_pass = "/home/pi/Music/akanechan/ohayou/"
 
-# ファイル名を取得
-files = os.listdir(music_pass)
-print("ファイル数:",str(len(files)))
-for file in files:
-    print("\t",file)
-    
-# 乱数で選曲
-r = random
-r.seed()
-play_no = r.randint(0, len(files)-1)
-print(play_no,"曲目を再生")
-file = files[play_no]
-
-fullpass = music_pass + file
-
+fullpass = getRandomVoiceFullFilepass(music_pass)
 args = ['omxplayer', '-o', 'alsa', fullpass]
 
 process = subprocess.Popen(args)
@@ -117,3 +105,5 @@ except KeyboardInterrupt:
     pass
 
 GPIO.cleanup()
+
+
